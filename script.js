0e458aeffc5161651d9faa86de516d1ed51a73fe
deondaci <deondaci1@gmail.com>
2023-11-30 07:59:12 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function funk(){
+    let b = querySelector('#komo');
+    b.style.color = 'red';
+    b.innerHTML = "I have changed!";
+}
+
+function sonl(){
+    document.getElementById('komo').style.color = 'green';
+}
